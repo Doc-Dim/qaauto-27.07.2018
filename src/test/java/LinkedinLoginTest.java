6c3622f0f2f99eadb5a96045731d0b215e0e3824
dmeln <dmelnichenkooo@gmail.com>
2018-08-08 20:16:06 +0300

    Lesson 4

--- src/test/java/LinkedinLoginTest.java
+++ src/test/java/LinkedinLoginTest.java
@@ -3,40 +3,77 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import static java.lang.Thread.sleep;
+import static org.testng.Assert.assertEquals;
+
 public class LinkedinLoginTest {
+    /*preconditions*/
+    WebDriver browser;
+    /*связать две переменние*/
+  @BeforeMethod
+   public void beforeMethod() {
+      browser = new FirefoxDriver();
+      browser.get("https://www.linkedin.com/");
+  }
+  /*close browser*/
+    @AfterMethod
+    public void afterMethod(){
+        browser.close();
+        /*выплняется после метода*/
+    }
+
     @Test
-    public void successfulllogintest () {
-        WebDriver browser = new FirefoxDriver();
-        browser.get("https://www.linkedin.com/");
-        WebElement userEmailField = browser.findElement(By.xpath("//*[@id=\"login-email\"]"));
+    public void successfulllogintest() throws InterruptedException {
 
-        WebElement userPasswordField = browser.findElement(By.xpath("//*[@id=\"login-password\"]"));
+       LinkedinLoginPage linkedinLoginPage = new LinkedinLoginPage(browser);
+       linkedinLoginPage.login("m.dimchik.test@gmail.com", "welcome123");
 
-        WebElement signInButton = browser.findElement(By.xpath("//*[@id=\"login-submit\"]"));
+        sleep(3000);
 
-        userEmailField.sendKeys("a@b.c");
-        userPasswordField.sendKeys("wrong");
-        signInButton.click();
+
+        //*Assert page Title after login
+        String pageTitle = browser.getTitle();
+        Assert.assertEquals(pageTitle, "LinkedIn", "Home page Title is wrong");
+        /*сравневает два обьекта*/
+
+        //*Assert some specific WebElement is displayed after login
+         WebElement profileNavigationItem = browser.findElement(By.xpath("//*[@id='profile-nav-item']"));
+        Assert.assertTrue(profileNavigationItem.isDisplayed(),"'profileNavigationItem' is not displayed on Home page");
+
+
+
+        //*Assert page URL after login
+        String pageUrl = browser.getCurrentUrl();
+        Assert.assertEquals(pageUrl, "https://www.linkedin.com/feed/","Home page url is wrong");
+
+
 
     }
+
+
+
+
     @Test
-    public void negativelogintest () throws InterruptedException {
-        WebDriver browser = new FirefoxDriver();
-        browser.get("https://www.linkedin.com/");
+    public void negativelogintest() throws InterruptedException {
+
         WebElement userEmailField = browser.findElement(By.xpath("//*[@id=\"login-email\"]"));
-
         WebElement userPasswordField = browser.findElement(By.xpath("//*[@id=\"login-password\"]"));
-
         WebElement signInButton = browser.findElement(By.xpath("//*[@id=\"login-submit\"]"));
-
         userEmailField.sendKeys("a@b.c");
         userPasswordField.sendKeys("wrong");
         signInButton.click();
-        Thread.sleep(4000);
+        sleep(4000);
         WebElement alertBox = browser.findElement(By.xpath("//*[@role='alert']"));
 
-        Assert.assertEquals(alertBox.getText(), "There were one or more errors in your submission. Please correct the marked fields below.", "Alert box has incorrect message");
+        assertEquals(alertBox.getText(),
+                "При заполнении формы были допущены ошибки. Проверьте и исправьте отмеченные поля.",
+                "Alert box has incorrect message");
     }
+
+
 }
+
